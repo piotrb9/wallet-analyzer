a4561e54f7a23680569a04b25ddbe7000e9d4f5d
piotrb <piotrbanko@me.com>
2023-11-19 21:05:29 +0100

    Changed cache time to 3 hours

--- src/download_wallet_txs.py
+++ src/download_wallet_txs.py
@@ -9,7 +9,7 @@ api_config.read('data/api_keys.ini')
 
 etherscan_api_key = api_config.get('api', 'etherscan_api_key')
 
-requests_cache.install_cache('cache/cache', backend='sqlite', expire_after=600)
+requests_cache.install_cache('cache/cache', backend='sqlite', expire_after=60 * 60 * 3)
 
 
 def get_txs(address: str, endpoint: str = 'etherscan.com', startblock: int = 0) -> list:
