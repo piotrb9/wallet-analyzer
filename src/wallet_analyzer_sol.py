33c0367ef82bc27d89388e71ae185e5a2c3da2b4
piotrb <piotrbanko@me.com>
2024-06-18 23:55:51 +0200

    Add solana config data

--- src/wallet_analyzer_sol.py
+++ src/wallet_analyzer_sol.py
@@ -1,108 +1,10 @@
 import json
+import os
+
 import pandas as pd
+import yaml
 
-
-def load_transactions(file_path, limit=100):
-    # Load the JSON file
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-    data = data['results']
-    return data[:limit]
-
-
-def get_swaps(transactions, wallet_address):
-    transactions_data = []
-
-    for txn in transactions:
-        transaction_hash = txn['transactionHash']
-        fees_paid = 0
-        is_swap = False
-        transfer_actions = []
-        tokens_involved = set()
-        sol_sent = 0
-        sol_received = 0
-        tokens_sent = 0
-        tokens_received = 0
-        timestamp = None
-        swap_type = None
-
-        for action in txn['data']:
-            if action['action'] == 'pay_tx_fees':
-                fees_paid = action['amount'] / 1000000000  # Convert fees to SOL
-
-            if action['action'] == 'transfer':
-                transfer_actions.append(action)
-                if action['token']:
-                    if action['token'] != "":
-                        tokens_involved.add(action['token'])
-
-                if action['token'] == "So11111111111111111111111111111111111111112":
-                    # For buy orders
-                    if action['source'] == wallet_address:
-                        sol_sent += action['amount']/1000000000
-                    elif action['destination'] == wallet_address:
-                        sol_received += action['amount']/1000000000
-
-                elif action['token'] != "":
-                    # For buy orders
-                    if action['destination'] == wallet_address:
-                        tokens_received += action['amount']
-                    elif action['source'] == wallet_address:
-                        tokens_sent += action['amount']
-
-                timestamp = action['timestamp']
-
-        if sol_sent > 0 and sol_received == 0:
-            swap_type = "buy"
-        elif sol_received > 0 and sol_sent == 0:
-            swap_type = "sell"
-
-        # token_amount = tokens_sent if tokens_sent > 0 else tokens_received
-
-        sol_amount = sol_received - sol_sent
-        token_amount = tokens_received - tokens_sent
-
-        price = (sol_amount * 1000000000) / token_amount if token_amount > 0 else None
-
-        token_traded_1 = list(tokens_involved)[0] if len(tokens_involved) > 0 else None
-        token_traded_2 = list(tokens_involved)[1] if len(tokens_involved) > 1 else None
-
-
-        transactions_data.append({
-            "transaction_hash": transaction_hash,
-            "timestamp": timestamp,
-            "is_swap": is_swap,
-            "fees": fees_paid,
-            "price": price,
-            "token_1": token_traded_1,
-            "token_2": token_traded_2,
-            "sol_sent": sol_sent,
-            "sol_received": sol_received,
-            "sol_amount": sol_amount,
-            "tokens_sent": tokens_sent,
-            "tokens_received": tokens_received,
-            "token_amount": token_amount,
-            "swap_type": swap_type
-        })
-
-    df = pd.DataFrame(transactions_data)
-
-    # Show the price column precisely
-    pd.set_option('display.float_format', lambda x: '%.9f' % x)
-
-    # Price column float32
-    df['price'] = df['price'].astype('float32')
-    return df
-
-
-# Use the function to load and process the first 100 transactions with detailed information and swap type
-file_path = '../temp/solana_fm_transfers.json'  # Update the file path as needed
-transactions = load_transactions(file_path)
-
-df_transactions_with_swap_type = get_swaps(transactions, wallet_address='4D168CJRDAM3SMpyh4tYCMVV2GqwehNhLvFd2ubHDhPY')
-
-# print(df_transactions_with_swap_type.head())
+from download_wallet_txs import SolanaDataDownloader
 
 
 class SolanaWalletAnalyzer:
@@ -111,103 +13,173 @@ class SolanaWalletAnalyzer:
         self.transactions = None
         self.swap_txs_df = None
 
+        # Download the transactions data
+        self.data_downloader = SolanaDataDownloader(self.wallet)
+
+        # Load the YAML file with os to make it work in the streamlit cloud
+        current_location = os.path.dirname(os.path.realpath(__file__))
+        yaml_file = os.path.join(current_location, 'data/contracts.yaml')
+        with open(yaml_file, 'r') as file:
+            config = yaml.safe_load(file)
+
+        self.stablecoins = config['bsc']['stablecoins']
+
     def load_transactions(self, file_path, limit=100):
         with open(file_path, 'r') as file:
             data = json.load(file)
         self.transactions = data['results'][:limit]
 
-    def classify_tx(self, from_wallet: str, to_wallet: str, method_id: str, value: float) -> str:
-        pass
+    def classify_tx(self, transaction, swap_type, swap_eth, traded_token) -> tuple:
+        tx_from = None
+        tx_to = None
+        tx_type = "other"
+        value = 0
 
-    def get_swap_type(self, txn_data):
-        sol_sent = 0
-        sol_received = 0
-        tokens_sent = 0
-        tokens_received = 0
+        token_transfers = transaction.get("tokenTransfers", [])
+
+        if swap_type in ["swap_buy", "swap_sell"]:
+            value = swap_eth
+
+            if traded_token[-4:] == "pump":
+                tx_type = "pumpfun_swap"
+            else:
+                tx_type = "swap"
+
+        elif transaction["type"] == "TRANSFER" and len(token_transfers) == 0:
+            value = transaction["nativeTransfers"][0]["amount"] / 10 ** 9
+            tx_from = transaction["nativeTransfers"][0]["fromUserAccount"]
+            tx_to = transaction["nativeTransfers"][0]["toUserAccount"]
+
+            if transaction["nativeTransfers"][0]["toUserAccount"] == self.wallet:
+                tx_type = "sol_transfer_in"
+
+            elif transaction["nativeTransfers"][0]["fromUserAccount"] == self.wallet:
+                tx_type = "sol_transfer_out"
+
+        elif transaction["type"] == "TRANSFER" and len(token_transfers) == 1 and token_transfers[0]["mint"] in self.stablecoins:
+            value = transaction["nativeTransfers"][0]["amount"]
+            tx_from = transaction["nativeTransfers"][0]["fromUserAccount"]
+            tx_to = transaction["nativeTransfers"][0]["toUserAccount"]
+
+            if token_transfers[0]["toUserAccount"] == self.wallet:
+                tx_type = "stablecoins_transfer_in"
+
+            elif token_transfers[0]["fromUserAccount"] == self.wallet:
+                tx_type = "stablecoins_transfer_out"
+
+        return tx_type, tx_from, tx_to, value
+
+    def get_swap_info(self, transaction) -> tuple:
+        # sol_sent = 0
+        # sol_received = 0
+        # tokens_sent = 0
+        # tokens_received = 0
+        # swap_type = None
+        # tokens_involved = set()
+        #
+        # for action in txn_data:
+        #     if action['action'] == 'pay_tx_fees':
+        #         fees_paid = action['amount'] / 1000000000  # Convert fees to SOL
+        #
+        #     if action['action'] == 'transfer':
+        #         # transfer_actions.append(action)
+        #         if action['token']:
+        #             if action['token'] != "":
+        #                 tokens_involved.add(action['token'])
+        #
+        #         if action['token'] == "So11111111111111111111111111111111111111112":
+        #             if action['source'] == self.wallet:
+        #                 sol_sent += action['amount'] / 1000000000
+        #             elif action['destination'] == self.wallet:
+        #                 sol_received += action['amount'] / 1000000000
+        #
+        #         elif action['token'] != "":
+        #             if action['destination'] == self.wallet:
+        #                 tokens_received += action['amount']
+        #             elif action['source'] == self.wallet:
+        #                 tokens_sent += action['amount']
+        #
+        # if sol_sent > 0 and sol_received == 0:
+        #     swap_type = "swap_buy"
+        # elif sol_received > 0 and sol_sent == 0:
+        #     swap_type = "swap_sell"
+        #
+        # sol_amount = sol_received - sol_sent
+        # token_amount = tokens_received - tokens_sent
+        #
+        # return swap_type, sol_amount, token_amount, tokens_involved
+
+        # -----------------------------
+        token_transfers = transaction.get("tokenTransfers", [])
+
         swap_type = None
-        tokens_involved = set()
+        from_token = None
+        to_token = None
+        from_token_amount = 0
+        to_token_amount = 0
+        sol_amount = 0
+        token_amount = 0
+        traded_token = None
 
-        for action in txn_data:
-            if action['action'] == 'pay_tx_fees':
-                fees_paid = action['amount'] / 1000000000  # Convert fees to SOL
+        if len(token_transfers) != 2:
+            print("Transaction does not have 2 token transfers")
+            return None, 0, 0, None
 
-            if action['action'] == 'transfer':
-                # transfer_actions.append(action)
-                if action['token']:
-                    if action['token'] != "":
-                        tokens_involved.add(action['token'])
+        # Find the token transfer involving the owner_address
+        for transfer in token_transfers:
+            if transfer["fromUserAccount"] == self.wallet:
+                from_token_amount = transfer["tokenAmount"]
+                from_token = transfer["mint"]
 
-                if action['token'] == "So11111111111111111111111111111111111111112":
-                    if action['source'] == self.wallet:
-                        sol_sent += action['amount'] / 1000000000
-                    elif action['destination'] == self.wallet:
-                        sol_received += action['amount'] / 1000000000
+            elif transfer["toUserAccount"] == self.wallet:
+                to_token_amount = transfer["tokenAmount"]
+                to_token = transfer["mint"]
 
-                elif action['token'] != "":
-                    if action['destination'] == self.wallet:
-                        tokens_received += action['amount']
-                    elif action['source'] == self.wallet:
-                        tokens_sent += action['amount']
-
-        if sol_sent > 0 and sol_received == 0:
+        # Determine if it's a buy or sell based on the transfers
+        if from_token == "So11111111111111111111111111111111111111112" and to_token != "So11111111111111111111111111111111111111112":
             swap_type = "swap_buy"
-        elif sol_received > 0 and sol_sent == 0:
+            sol_amount = from_token_amount
+            token_amount = to_token_amount
+            traded_token = to_token
+        elif to_token == "So11111111111111111111111111111111111111112" and from_token != "So11111111111111111111111111111111111111112":
             swap_type = "swap_sell"
+            sol_amount = to_token_amount
+            token_amount = from_token_amount
+            traded_token = from_token
 
-        sol_amount = sol_received - sol_sent
-        token_amount = tokens_received - tokens_sent
-
-        return swap_type, sol_amount, token_amount, tokens_involved
+        return swap_type, sol_amount, token_amount, traded_token
 
     def get_data(self):
+        # Download the transactions data
+        self.transactions = self.data_downloader.get_txs_helius()
+
         # Load transactions data
         txs_list = []
-        swap_txs_list = []
+        # swap_txs_list = []
 
         for txn in self.transactions:
 
-            fees_paid = 0
-            is_swap = False
-            swap_type = None
-
-            swap_type, sol_amount, token_amount, tokens_involved = self.get_swap_type(txn['data'])
-            price = (sol_amount * 1000000000) / token_amount if token_amount > 0 else None
-
-            token_traded_1 = list(tokens_involved)[0] if len(tokens_involved) > 0 else None
-            token_traded_2 = list(tokens_involved)[1] if len(tokens_involved) > 1 else None
-
-            token_traded = token_traded_1 if token_traded_2 == "So11111111111111111111111111111111111111112" else token_traded_2
-
-            # swap_txs_list.append({
-            #     "transaction_hash": txn['transactionHash'],
-            #     "timestamp": timestamp,
-            #     "is_swap": is_swap,
-            #     "fees": fees_paid,
-            #     "price": price,
-            #     "token_1": token_traded_1,
-            #     "token_2": token_traded_2,
-            #     "sol_amount": sol_amount,
-            #     "token_amount": token_amount,
-            #     "swap_type": swap_type
-            #
-            # })
+            swap_type, sol_amount, token_amount, traded_token = self.get_swap_info(txn)
+            tx_type, tx_from, tx_to, value = self.classify_tx(txn, swap_type, sol_amount, traded_token)
+            price = sol_amount / token_amount if token_amount != 0 else None
 
             # Fill the txs_list
             txs_list.append({
-                "hash": txn['transactionHash'],
+                "hash": txn['signature'],
                 # "blockNumber": txn['blockNumber'],
-                "timeStamp": txn['data'][0]['timestamp'],
-                "fees": fees_paid,
-                "txType": None,
-                "from": None,
-                "to": None,
-                "value": sol_amount,
+                "timeStamp": txn['timestamp'],
+                "txFee": txn['fee']/10**9,
+                "txType": tx_type,
+                "from": tx_from,
+                "to": tx_to,
+                "value": value,
                 "swapType": swap_type,
                 "swapEth": abs(sol_amount),
+                "price": price,
                 "tokenValue": abs(token_amount),
-                "tokenName": token_traded,
+                "tokenName": traded_token,
                 "tokenSymbol": None,
-                "tokenCa": token_traded,
+                "tokenCa": traded_token,
                 "tokenDecimal": 12.0,
                 "gasPrice": 1,
                 "gasUsed": 1,
@@ -215,7 +187,7 @@ class SolanaWalletAnalyzer:
             })
 
         self.txs_df = pd.DataFrame(txs_list)
-        self.swap_txs_df = pd.DataFrame(swap_txs_list)
+        # self.swap_txs_df = pd.DataFrame(swap_txs_list)
         # return self.swap_txs_df
 
     def get_swap_txs(self) -> pd.DataFrame:
